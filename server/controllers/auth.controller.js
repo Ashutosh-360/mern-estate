b5759efaa41a55431f80117c7671f4e60f844d6b
Ashutosh Naik <ashutoshnaik360@gmail.com>
2023-10-28 23:11:59 +0530

    hashed password

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,10 +1,13 @@
 const User = require("../models/user.model.js");
+const bcryptjs=require("bcryptjs");
 
 module.exports = signup = async (req, res) => {
   const { username, email, password } = req.body;
 
-  const user = new User({ username, email, password });
-  
+  const hashedPassword=bcryptjs.hashSync(password,10);
+  const user = new User({ username, email, password:hashedPassword });
+
+  //here 10 is salt number which is default
   try {
       await user.save();
       res.status(200).send({ success: true, message: "User Saved successfully" });
