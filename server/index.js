edfab0b0b15c52c35e2abc9bcfb33a450b2a4aeb
Ashutosh Naik <ashutoshnaik360@gmail.com>
2023-10-28 10:25:31 +0530

    git ignore

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(8000,() => {
-    console.log("port running on")
-})
+mongoose.connect("mongodb://localhost:27017/real-estate").then(() => {
+  console.log("Mongoose connected");
+});
+app.listen(8000, () => {
+  console.log("port running on");
+});
